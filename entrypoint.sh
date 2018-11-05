c2f5d69d85eaeadfa07ccbd1911cd942c995622b
wanghongkai <wanghongkai@corp.netease.com>
2018-11-05 10:12:07 +0800

    add network restriction

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# limit network
+tc qdisc add dev eth0 root tbf rate 4mbit peakrate 8mbit burst 64kb latency 50ms minburst 1540
+
 # first arg is `-f` or `--some-option`
 if [ "${1#http}" != "$1" ]; then
     set -- lighthouse --enable-error-reporting --chrome-flags="--headless --disable-gpu" "$@"
