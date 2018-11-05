c3ef145bd7dce1625186adc02e3f0500b577caa4
reditaru <reditaru1997@gmail.com>
2018-11-05 15:20:55 +0800

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,12 @@ tc qdisc add dev eth0 root tbf rate 4mbit peakrate 8mbit burst 64kb latency 50ms
 
 # first arg is `-f` or `--some-option`
 if [ "${1#http}" != "$1" ]; then
-    set -- lighthouse --enable-error-reporting --chrome-flags="--headless --disable-gpu" "$@"
+    command="sudo -H -u chrome sh -c \"lighthouse --enable-error-reporting --chrome-flags=\\\"--headless --disable-gpu\\\" $@\""
+    exec sh -c "$command"
+else
+    # pass extra command at position 0, such as network limit (tc qdisc add dev eth0 root tbf rate 4mbit peakrate 8mbit burst 64kb latency 50ms minburst 1540)
+    first_arg="$1"
+    shift
+    command="$first_arg && sudo -H -u chrome sh -c \"lighthouse --enable-error-reporting --chrome-flags=\\\"--headless --disable-gpu\\\" $@\""
+    exec sh -c "$command"
 fi
-
-exec "$@"
